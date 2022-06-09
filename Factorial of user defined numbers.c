6cfbacb672b6952a32d9665b7e404c7d93d44fae
Pratik Raut <106788839+MrCodingJunky-jr@users.noreply.github.com>
2022-06-09 06:55:47 +0545

    Add files via upload

---
+++ Factorial of user defined numbers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+	int fact=1,a,n;
+	printf("Enter the number for which you want to find the factorial of :\n");
+	scanf("%d",&n);
+	for(a=1;a<=n;a++){
+		fact=fact*a;
+	}
+	printf("\nFactorial is %d",fact);
+	return 0;
+}
+
